8b0e7bb0c210cfe19870936d7504966e8006471f
dang200803 <huynhhaidang.20082003@gmail.com>
2023-09-20 19:42:38 +0700

    Edit code cua file

---
+++ Class1.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test.codeusing System;
+
+{
+    public class Hello_World
+{
+    public static void main(string[] args)
+    {
+        Console.WriteLine("Hello_World");
+    }
+}
+}
